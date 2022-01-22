06938d00d4a4573f32ee5372aa4b5cfc48babef6
Aidan Gomez <44831139+atomdog@users.noreply.github.com>
2022-01-22 17:12:29 -0500

    Create gpio_interface.py

---
+++ gpio_interface.py
@@ -0,0 +1,33 @@
+#gpio_interface.py
+import RPi.GPIO as GPIO
+
+class geep():
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        self.configured_inputs = []
+        self.configured_outputs = []
+        self.current_input_readings = []
+    def config_outputs(self, pin_list):
+        for x in range(0, len(pinlist)):
+            GPIO.setup(pinlist[x],GPIO.OUT)
+            self.configured_outputs.append(pinlist[x])
+    def config_inputs(self, pin_list):
+        for x in range(0, len(pinlist)):
+            GPIO.setup(pinlist[x],GPIO.IN)
+            self.configured_inputs.append(pinlist[x])
+    def batch_read_input(self):
+        self.current_input_readings = []
+        for x in range(0, len(self.configured_inputs)):
+            self.current_input_readings.append(GPIO.input(self.config_inputs[x]))
+    def highlow_batch_send_output(self, outputlist):
+        if(len(outputlist!=len(self.configured_outputs))):
+            return(False)
+        else:
+            for x in range(0, len(outputlist)):
+                if(outputlist[x]==1):
+                    GPIO.output(self.configured_outputs[x],GPIO.HIGH)
+                else:
+                    GPIO.output(self.configured_outputs[x],GPIO.LOW)
+    def clean(self):
+        GPIO.cleanup()
